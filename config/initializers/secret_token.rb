2c3cef147b44d9a6ecb4835f38ce61a9ea7131de
Stephanie Entinger <sentinger53@gmail.com>
2014-03-10 16:48:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Launderlist::Application.config.secret_key_base = '85f3b981e24ff23a6be8a30b58c8ae8b2f4f291c69766b05a99b2cdfe8775ed60bfe4896d93d80cecbc5965a5ea2dbca04ec173d62bfbdde59dbb050a11518ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Launderlist::Application.config.session_store :cookie_store, key: '_launderlist_session'
